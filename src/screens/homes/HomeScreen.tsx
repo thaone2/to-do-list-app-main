3676e7ed8e8f21ab0e4957bde92f48c217002fe8
thaone2 <khuong123w@gmail.com>
2024-09-28 18:42:27 +0700

    update: Update ui for HomeScreen

--- src/screens/homes/HomeScreen.tsx
+++ src/screens/homes/HomeScreen.tsx
@@ -7,7 +7,7 @@ import {
   SearchNormal1,
 } from 'iconsax-react-native';
 import React from 'react';
-import {TouchableOpacity, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import AvatarGroup from '../../components/AvatarGroup';
 import CardComponent from '../../components/CardComponent';
 import CardImageConponent from '../../components/CardImageConponent';
@@ -24,35 +24,221 @@ import {colors} from '../../constants/colors';
 import {fontFamilies} from '../../constants/fontFamilies';
 import {globalStyles} from '../../styles/globalStyles';
 import auth from '@react-native-firebase/auth';
+import {Dimensions} from 'react-native';
 
 const HomeScreen = ({navigation}: any) => {
   const handleSingout = async () => {
     await auth().signOut();
   };
 
+  const screenWidth = Dimensions.get('window').width;
+
   return (
     <View style={{flex: 1}}>
       <Container isScroll>
-        <SectionComponent>
+        {/* <SectionComponent>
           <RowComponent justify="space-between">
             <Element4 size={24} color={colors.desc} />
-            <Notification size={24} color={colors.desc} />
+            <Notification size={24} color={colors.desc} /> 
           </RowComponent>
-        </SectionComponent>
+        </SectionComponent> */}
         <SectionComponent>
           <RowComponent>
-            <View
+            {/* <View
               style={{
                 flex: 1,
               }}>
               <TextComponent text="Hi, Jason" />
               <TitleComponent text="Be Productive today" />
+            </View> */}
+            <View
+              style={{
+                flex: 1,
+              }}>
+              <TextComponent text="HomeScreen" />
             </View>
             <TouchableOpacity onPress={handleSingout}>
               <Logout size={22} color="coral" />
             </TouchableOpacity>
           </RowComponent>
         </SectionComponent>
+
+        <SectionComponent styles={{flex: 1, flexDirection: 'row'}}>
+          <View
+            style={[
+              globalStyles.inputContainer,
+              {
+                flex: 1,
+                width: screenWidth - 40,
+                // height: 140,
+                justifyContent: 'center',
+                alignItems: 'center',
+              },
+            ]}>
+            <TextComponent
+              styles={[
+                {
+                  color: '#26F2CD',
+                  fontWeight: 'bold',
+                  // backgroundColor: 'gray',
+                  // paddingHorizontal: 100,
+                  // paddingVertical: 40,
+                  // marginBottom: 10,
+                },
+              ]}
+              text="HLK Radar Sensor"
+            />
+            {/* <TextComponent text="Trạng thái cảm biến" /> */}
+
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 50 / 2,
+                backgroundColor: '#5DF15A',
+                marginTop: 4,
+              }}></View>
+
+            {/* <TouchableOpacity>
+              <TextComponent
+                styles={[
+                  globalStyles.inputContainer,
+                  {
+                    color: 'green',
+                    fontWeight: 'bold',
+                    paddingHorizontal: 40,
+                    paddingVertical: 40,
+                  },
+                ]}
+                text="HLK Radar Sensor"
+              />
+            </TouchableOpacity> */}
+          </View>
+        </SectionComponent>
+
+        <SectionComponent styles={{flex: 1, flexDirection: 'row'}}>
+          <View
+            style={[
+              globalStyles.inputContainer,
+              {
+                flex: 1,
+                width: screenWidth - 40,
+                // height: 140,
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginRight: 10,
+              },
+            ]}>
+            <TextComponent text="Computer 1" />
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 50 / 2,
+                backgroundColor: '#EC342A',
+                marginTop: 4,
+              }}></View>
+          </View>
+          <View
+            style={[
+              globalStyles.inputContainer,
+              {
+                flex: 1,
+                width: screenWidth - 40,
+                // height: 140,
+                justifyContent: 'center',
+                alignItems: 'center',
+              },
+            ]}>
+            <TextComponent text="Computer 2" />
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 50 / 2,
+                backgroundColor: '#5DF15A',
+                marginTop: 4,
+              }}></View>
+          </View>
+        </SectionComponent>
+        <SectionComponent styles={{flex: 1, flexDirection: 'row'}}>
+          <View
+            style={[
+              globalStyles.inputContainer,
+              {
+                flex: 1,
+                width: screenWidth - 40,
+                // height: 140,
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginRight: 10,
+              },
+            ]}>
+            <TextComponent text="Computer 3" />
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 50 / 2,
+                backgroundColor: '#5DF15A',
+                marginTop: 4,
+              }}></View>
+          </View>
+          <View
+            style={[
+              globalStyles.inputContainer,
+              {
+                flex: 1,
+                width: screenWidth - 40,
+                // height: 140,
+                justifyContent: 'center',
+                alignItems: 'center',
+              },
+            ]}>
+            <TextComponent text="Computer 4" />
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 50 / 2,
+                backgroundColor: '#5DF15A',
+                marginTop: 4,
+              }}></View>
+          </View>
+        </SectionComponent>
+
+        <SectionComponent styles={{flex: 1, flexDirection: 'row'}}>
+          <View
+            style={[
+              globalStyles.inputContainer,
+              {
+                flex: 1,
+                width: screenWidth - 40,
+                // height: 140,
+                justifyContent: 'center',
+                alignItems: 'flex-start',
+                marginLeft: 2,
+              },
+            ]}>
+            <TextComponent
+              styles={{fontWeight: 'bold', fontSize: 15, color: '#9A7FFE'}}
+              text="Lượng điện tiêu thụ"
+            />
+            <TextComponent
+              styles={{marginLeft: 2}}
+              text="Lượng điện tiêu thụ hàng ngày: 10 kWh"
+            />
+            <TextComponent
+              styles={{marginLeft: 2}}
+              text="Lượng điện tiêu thụ hàng tuần: 70 kWh"
+            />
+            <TextComponent
+              styles={{marginLeft: 2}}
+              text="Lượng điện tiêu thụ hàng tháng: 210 kWh"
+            />
+          </View>
+        </SectionComponent>
+
         <SectionComponent>
           <RowComponent
             styles={[globalStyles.inputContainer]}
@@ -81,7 +267,7 @@ const HomeScreen = ({navigation}: any) => {
             </RowComponent>
           </CardComponent>
         </SectionComponent>
-        <SectionComponent>
+        {/* <SectionComponent>
           <RowComponent styles={{alignItems: 'flex-start'}}>
             <View style={{flex: 1}}>
               <CardImageConponent>
@@ -133,8 +319,8 @@ const HomeScreen = ({navigation}: any) => {
               </CardImageConponent>
             </View>
           </RowComponent>
-        </SectionComponent>
-        <SectionComponent>
+        </SectionComponent> */}
+        {/* <SectionComponent>
           <TextComponent
             flex={1}
             font={fontFamilies.bold}
@@ -150,7 +336,7 @@ const HomeScreen = ({navigation}: any) => {
               </View>
             </RowComponent>
           </CardComponent>
-        </SectionComponent>
+        </SectionComponent> */}
       </Container>
       <View
         style={{
